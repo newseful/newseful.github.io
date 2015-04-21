46125b51b13e89c98dd1faedcf3ae4ddaaf19ee4
Kevin Zweerink <kevin@kevinzweerink.com>
2015-04-21 16:47:02 -0400

    restructure, add documentation, move typography specification out of newseful.css

--- README.md
+++
@@ -1,7 +0,0 @@
-Agendae is an exploratory project seeking new models for the display and dissemination of developing news stories on the internet. The explicit goal of the project is finding patterns that:
-
-1. Reduce the spread of misreported facts and speculation,
-2. Make it easier to keep track of a developing story across multiple device contexts, and
-3. Leverage the inherent qualities of digital media to enhance understanding
-
-[Blog](http://agendae.github.io) | [Reference](http://agendae.github.io/reference/) | [Research](https://github.com/agendae/research) | [Who](http://kevinzweerink.com)
--- js/main.js
+++ js/main.js
@@ -1,16 +1,49 @@
-window.onload = function(e) {
-	var links = document.querySelectorAll('.archive a');
+if (window.location.pathname.match('docs')) {
+	NodeList.prototype.forEach = Array.prototype.forEach;
+	NodeList.prototype.reduce = Array.prototype.reduce;
 
-	for (var i = 0; i < links.length; ++i) {
-		var a = links[i];
-		a.addEventListener('click', function(e) {
-			e.preventDefault();
-			var href = this.getAttribute('href').substr(1);
-			var query = document.getElementById(href);
-			document.querySelector('body').scrollTop = query.getBoundingClientRect().top - 80;
-		});
+	function updateCurrentIndicator() {
+		var breaker = false;
+		var currentPosition = links.reduce(function(a, l) {
+			l.classList.remove('panel__navigation-link--current');
+			
+			if (breaker)
+				return a;
+
+			var section = docReader.querySelector(l.getAttribute('href'));
+			if (section.offsetTop - 64 <= docReader.scrollTop) {
+				++a;
+			}
+
+			if (docReader.scrollTop >= docReader.scrollHeight - section.getBoundingClientRect().height - 50) {
+				a = links.length - 1;
+				breaker = true;
+			}
+
+			return a;
+		}, -1);
+
+		if (currentPosition < 0)
+			currentPosition = 0;
+
+		links[currentPosition].classList.add('panel__navigation-link--current');
 	}
 
-	var visualizationInit = new Event('init');
-	window.dispatchEvent(visualizationInit);
-};
+	var docReader = document.querySelector('.left-panel');
+	var navigation = docReader.querySelector('.panel__navigation-list');
+	var links = docReader.querySelectorAll('.panel__navigation-link');
+
+	links.forEach(function(l) {
+		l.addEventListener('click', function(e) {
+			e.preventDefault();
+			var offset = docReader.querySelector(this.getAttribute('href')).offsetTop;
+			docReader.scrollTop = offset - 64;
+
+			updateCurrentIndicator();
+		});
+	});
+
+	docReader.addEventListener('scroll', function(e) {
+		updateCurrentIndicator();
+	});
+}
